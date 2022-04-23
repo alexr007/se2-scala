a5f587efa25f902bd8abb719088976da0259d7bb
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-04-23 12:26:05 +0300

    lesson3

---
+++ src/main/scala/l08/FPExample.scala
@@ -0,0 +1,19 @@
+package l08
+
+object FPExample extends App {
+
+  def printMessage1(message: String) =
+    println(message)
+
+  def printMessage2(message: String) =
+    () => println(message)
+
+  val m1 = printMessage2("hello")
+  val m2 = printMessage2("world")
+  val m3: () => Unit = () => {
+    m1()
+    m2()
+  }
+  m3()
+
+}
